4b20c1f41fe0dec8f660c005bcf82a015ecf5595
Vivian Luxon <arterinv@gmail.com>
2024-03-29 14:37:19 +1300

    front end functionality almost done

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,36 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct OutputFields {
+    integration: i32,
+    scope: i32,
+    schedule: i32,
+    cost: i32,
+    quality: i32,
+    resource: i32,
+    communications: i32,
+    risk: i32,
+    procurement: i32,
+    stakeholder: i32,
+    total: i32,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn score() {
+    
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![score])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -3,16 +3,39 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
-async function greet() {
+// async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  // greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+
+async function score() {
+  var form = document.querySelector("#form-container");
+  var inputs = {
+    integration: form.elements["Integration"].value,
+    scope: form.elements["Scope"].value,
+    schedule: form.elements["Schedule"].value,
+    cost: form.elements["Cost"].value,
+    quality: form.elements["Quality"].value,
+    resource: form.elements["Resource"].value,
+    communications: form.elements["Communications"].value,
+    risk: form.elements["Risk"].value,
+    procurement: form.elements["Procurement"].value,
+    stakeholder: form.elements["Stakeholder"].value
+  }
+
+  //await invoke("score", {});
+
+
+  console.log("hewwo\n");
+  console.log(inputs);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  // greetInputEl = document.querySelector("#greet-input");
+  // greetMsgEl = document.querySelector("#greet-msg");
+  
+  document.querySelector("#form-container").addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    score();
   });
 });
